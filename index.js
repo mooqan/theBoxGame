4e9292cb4c6191d7526bce65ab9c2710210b0055
mooqan <districkwar@gmail.com>
2023-11-29 16:21:46 +0600

    commit

--- index.js
+++ index.js
@@ -1,16 +1,73 @@
-var $start = document.querySelector('#start')
-var $game = document.querySelector('#game')
+var $start = document.querySelector("#start");
+var $game = document.querySelector("#game");
+var $time = document.querySelector("#time");
+var $timeHeader = document.querySelector("#time-header");
+var $resultHeader = document.querySelector("#result-header");
 
-$start.addEventListener("click", startGame)
+var score = 0;
+var isGameStarted = false;
 
+$start.addEventListener("click", startGame);
+$game.addEventListener("click", handleBoxClick);
 
 function startGame() {
-    $game.style.backgroundColor = '#fff'
-    $start.classList.add('hide')
+  isGameStarted = true;
+  $game.style.backgroundColor = "#fff";
+  $start.classList.add("hide");
 
-    renderBox()
+  var interval = setInterval(function () {
+    var time = parseFloat($time.textContent);
+    if (time <= 0) {
+      clearInterval(interval);
+      endGame();
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1);
+    }
+  }, 100);
+
+  renderBox();
+}
+
+function endGame() {
+  isGameStarted = false;
+  $start.classList.remove("hide");
+  $game.innerHTML = "";
+  $game.style.backgroundColor = "#ccc";
+  $timeHeader.classList.add("hide");
+  $resultHeader.classList.remove("hide");
+}
+
+function handleBoxClick(event) {
+  if (!isGameStarted) {
+    return;
+  }
+
+  if (event.target.dataset) {
+    score++;
+    renderBox();
+  }
 }
 
 function renderBox() {
- 
+  $game.innerHTML = "";
+  var box = document.createElement("div");
+  var boxSize = getRandom(30, 100);
+  var gameSize = $game.getBoundingClientRect();
+  var maxTop = gameSize.height - boxSize;
+  var maxLeft = gameSize.width - boxSize;
+  console.log(gameSize);
+
+  box.style.height = box.style.width = boxSize + "px";
+  box.style.position = "absolute";
+  box.style.backgroundColor = "#000";
+  box.style.top = getRandom(0, maxTop) + "px";
+  box.style.left = getRandom(0, maxLeft) + "px";
+  box.style.cursor = "pointer";
+  box.setAttribute("data-box", "true");
+
+  $game.insertAdjacentElement("afterbegin", box);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
